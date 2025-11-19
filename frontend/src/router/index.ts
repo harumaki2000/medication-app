c0fb73e4ab225e6d9062a7372ccf86553044ac97
harumaki2000 <harumaki2000.net@gmail.com>
2025-11-19 14:10:29 +0900

    feat: ログイン画面の完成

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,21 @@
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+  ],
+})
+
+export default router
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="card">
+    <h1>ログイン</h1>
+
+    <div class="form-group">
+      <label for="email">メールアドレス</label>
+      <input type="email" id="email" v-model="email"/>
+    </div>
+
+    <div class="form-group">
+      <label for="password">パスワード</label>
+      <input type="password" id="password" v-model="password"/>
+    </div>
+
+    <button class="primary-button" @click="login">ログイン</button>
+    <button class="secondary-button" @click="goToRegister">新規登録</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const email = ref('')
+const password = ref('')
+
+const login = () => {
+  alert('ログイン処理')
+}
+
+const goToRegister = () => {
+  router.push('/register')
+}
+</script>
+
+<style scoped>
+body {
+  font-family: 'Arial', sans-serif;
+  background-color: #f0f2f5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  margin: 0;
+}
+
+.container {
+  max-width: 400px;
+  width: 100%;
+  padding: 30px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  text-align: center;
+}
+
+h1 {
+  color: #333;
+  margin-bottom: 30px;
+  font-size: 24px;
+}
+
+.form-group {
+  margin-bottom: 20px;
+  text-align: left;
+}
+
+label {
+  display: block;
+  margin-bottom: 8px;
+  font-weight: bold;
+  color: #555;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+  width: calc(100% - 20px);
+  padding: 10px;
+  border: 1px solid #555;
+  border-radius: 5px;
+  font-size: 16px;
+}
+
+input:focus {
+  border-color: #007bff;
+  outline: none;
+  box-shadow: 0 0 0 3px rgba(0, 123, 255, 0.25);
+}
+
+button {
+  width: 100%;
+  padding: 12px;
+  border: none;
+  border-radius: 5px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  margin-top: 10px;
+}
+
+.primary-button {
+  background-color: #e0e0e0;
+  color: #333;
+}
+
+.primary-button:hover {
+  background-color: #d0d0d0;
+}
+
+.secondary-button {
+  background-color: #627fce;
+  color: #ffffff;
+}
+
+.secondary-button:hover {
+  background-color: #4c6fce;
+}
+</style>
